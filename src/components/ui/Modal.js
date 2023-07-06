e4969726f06a1fd67e14968a168f8e1951781cff
dileepkumar <gurramdileepkumar96@gmail.com>
2023-07-06 10:36:59 +0530

    second commit

--- src/components/ui/Modal.js
+++ src/components/ui/Modal.js
@@ -6,9 +6,9 @@ const Backdrop=(props)=>{
    return <div className={classes.backdrop} onClick={props.onClick}></div>
 }
 const ModalOverlay=(props)=>{
-    return <div className={classes.modal}>
+    return (<div className={classes.modal}>
         <div className={classes.content}>{props.children}</div>
-    </div>
+    </div>)
 }
 const portalElement=document.getElementById('overlays');
 const Modal = (props) => {
